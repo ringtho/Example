c54e35ee96af2b087358540274b549ceebe2b934
Smith Ringtho <sringtho@gmail.com>
2019-01-02 13:02:37 +0300

    add links and display all RSS feeds as opposed to one

--- headlines.py
+++ headlines.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 import feedparser
 
 app = Flask(__name__)
@@ -22,16 +22,20 @@ RSS_FEEDS = {
 
 def get_news(publication="bbc"):
 	feed = feedparser.parse(RSS_FEEDS[publication])
+	feeds = feed['entries']
 	first_article = feed['entries'][0]
-	return """<html>
-		<body>
-			<h1> News Headlines </h1>
-			<b>{0}</b> <br/>
-			<i>{1}</i> <br/>
-			<p>{2}</p> <br/>
-		</body>
-	</html>""".format(first_article.get("title"), first_article.
-	get("published"), first_article.get("summary"))
+	return render_template("home.html", articles=feeds)
+	# return render_template('home.html', title=first_article.get("title"),
+	# 	published=first_article.get("published"), summary=first_article.get("summary"))
+	# return """<html>
+	# 	<body>
+	# 		<h1> News Headlines </h1>
+	# 		<b>{0}</b> <br/>
+	# 		<i>{1}</i> <br/>
+	# 		<p>{2}</p> <br/>
+	# 	</body>
+	# </html>""".format(first_article.get("title"), first_article.
+	# get("published"), first_article.get("summary"))
 	# return 'No news is good news!!'
 
 if __name__ == '__main__':
